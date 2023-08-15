6d49a8c49ec456b79b4342ff300cc9c25f99bc41
Kidd254 <lorenkioko@gmail.com>
2023-08-15 20:56:35 +0300

    Add querries for specific conditions

--- queries.sql
+++ queries.sql
@@ -34,3 +34,33 @@ WHERE weight_kg < 0;
 COMMIT;
 
 
+-- Queries to answer specific questions
+
+-- Count the total number of animals
+SELECT COUNT(*) AS total_animals FROM animals;
+
+-- Count the number of animals that have never tried to escape
+SELECT COUNT(*) AS num_no_escape_attempts
+FROM animals
+WHERE escape_attempts = 0;
+
+-- Calculate the average weight of animals
+SELECT AVG(weight_kg) AS avg_weight FROM animals;
+
+-- Find out who escapes the most, neutered or not neutered animals
+SELECT neutered, SUM(escape_attempts) AS total_escape_attempts
+FROM animals
+GROUP BY neutered
+ORDER BY total_escape_attempts DESC
+LIMIT 1;
+
+-- Find the minimum and maximum weight of each type of animal
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight
+FROM animals
+GROUP BY species;
+
+-- Calculate the average number of escape attempts per animal type of those born between 1990 and 2000
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
