da753044e825ed297be364615f1bbfc97d4ba38a
Kidd254 <lorenkioko@gmail.com>
2023-08-15 20:04:18 +0300

    Add a species column and add more animals

--- data.sql
+++ data.sql
@@ -16,3 +16,33 @@ VALUES ('Pikachu', '2021-01-07', 1, FALSE, 15.04);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Devimon', '2017-05-12', 5, TRUE, 11.00);
 
+-- Add a column 'species' of type string to the 'animals' table.
+ALTER TABLE animals ADD COLUMN species VARCHAR(255);
+
+-- Animal: His name is Charmander.
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES ('Charmander', '2020-02-08', 0, FALSE, -11.0);
+
+-- Animal: Her name is Plantmon.
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES ('Plantmon', '2021-11-15', 2, TRUE, -5.7);
+
+-- Animal: His name is Squirtle.
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES ('Squirtle', '1993-04-02', 3, FALSE, -12.13);
+
+-- Animal: His name is Angemon.
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES ('Angemon', '2005-06-12', 1, TRUE, -45.0);
+
+-- Animal: His name is Boarmon.
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES ('Boarmon', '2005-06-07', 7, TRUE, 20.4);
+
+-- Animal: Her name is Blossom.
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES ('Blossom', '1998-10-13', 3, TRUE, 17.0);
+
+-- Animal: His name is Ditto.
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+VALUES ('Ditto', '2022-05-14', 4, TRUE, 22.0);
